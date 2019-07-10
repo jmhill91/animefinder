2af28e1a41286b0b278c8886ad5c2e2551f49a99
jmhill91 <john.m.hill91@gmail.com>
2019-07-10 15:42:03 -0400

    my bad

--- anime_finder_backend/app/controllers/ani_gens_controller.rb
+++ anime_finder_backend/app/controllers/ani_gens_controller.rb
@@ -1,2 +1,6 @@
 class AniGensController < ApplicationController
+  def index
+    ani_gens = AniGen.all
+    render json: ani_gens
+  end
 end
--- anime_finder_backend/config/routes.rb
+++ anime_finder_backend/config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   post "/login", to: "auth#login"
   post "/signup", to: "users#create"
   get "/profile", to: "users#profile"
-
+  get "/anime-genres", to: "ani_gens#index"
 end
